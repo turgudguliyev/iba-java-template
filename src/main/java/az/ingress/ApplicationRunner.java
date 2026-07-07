bff20bd1db0f0c0e8ed636166bbe016225b33393
Turgud F. Guliyev <GuliyevTF>
2026-07-07 14:07:21 +0400

    feat: add Logger.java

--- src/main/java/az/ingress/ApplicationRunner.java
+++ src/main/java/az/ingress/ApplicationRunner.java
@@ -1,11 +1,18 @@
 package az.ingress;
 
 import az.ingress.cache.Storage;
+import az.ingress.logging.Logger;
 
 public class ApplicationRunner {
 
   public static void main(String[] args) {
     runCache();
+    runLog();
+  }
+
+  private static void runLog() {
+    Logger.log("USER_LOGIN", "username=hey", "password=1234");
+    Logger.log("C2C", "source=1234", "target=4321");
   }
 
   private static void runCache() {
